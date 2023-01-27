991b28d397e96b56aeea135daa3caa896485898a
DanielXOO <dankulinkovich@gmail.com>
2023-01-27 15:32:17 +0300

    add swagger

--- Warehouse.Api/Program.cs
+++ Warehouse.Api/Program.cs
@@ -1,4 +1,31 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(option =>
+{
+    option.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Warehouse.Api",
+        Description = "Application demonstrating saga pattern",
+        Contact = new OpenApiContact
+        {
+            Name = "GitHub",
+            Url = new Uri("https://github.com/DanielXOO/")
+        }
+    });
+});
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.Run();
